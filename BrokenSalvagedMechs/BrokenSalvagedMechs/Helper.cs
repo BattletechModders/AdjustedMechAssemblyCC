bcd04a282b45b13179418b14575de9113799663a
Clinton Mead <clintonmead@gmail.com>
2019-01-06 03:24:04 +1100

    Add option to have different parts requirement for different mechs, based on weight.

--- BrokenSalvagedMechs/BrokenSalvagedMechs/Helper.cs
+++ BrokenSalvagedMechs/BrokenSalvagedMechs/Helper.cs
@@ -1,21 +1,84 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using BattleTech;
 
 namespace AdjustedMechAssembly {
-    public class Helper {
-
-        public static Settings LoadSettings() {
-            try {
-                using (StreamReader r = new StreamReader($"{AdjustedMechAssembly.ModDirectory}/settings.json")) {
-                    string json = r.ReadToEnd();
-                    return JsonConvert.DeserializeObject<Settings>(json);
+    public static class Helper
+    {
+        private static Settings _settings;
+        public static Settings Settings
+        {
+            get
+            {
+                try
+                {
+                    if (_settings == null)
+                    {
+                        using (StreamReader r = new StreamReader($"{AdjustedMechAssembly.ModDirectory}/settings.json"))
+                        {
+                            string json = r.ReadToEnd();
+                            _settings = JsonConvert.DeserializeObject<Settings>(json);
+                        }
+                    }
+                    return _settings;
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError(ex);
+                    return null;
                 }
             }
-            catch (Exception ex) {
-                Logger.LogError(ex);
-                return null;
+        }
+
+        private static List<int> _partsByWeightTable;
+
+        public static List<int> PartsByWeightTable
+        {
+            get
+            {
+                if (_partsByWeightTable == null)
+                {
+                    _partsByWeightTable = new List<int>();
+                    int tonnage = 5;
+                    int partsRequired = 1;
+                    foreach (int cutoff in Settings.WeightThresholds)
+                    {
+                        while (tonnage <= cutoff)
+                        {
+                            _partsByWeightTable.Add(partsRequired);
+                            tonnage += 5;
+                        }
+
+                        ++partsRequired;
+                    }
+                }
+
+                return _partsByWeightTable;
             }
+        }
+
+        public static int GetMechPartsRequiredByWeight(ChassisDef chassisDef)
+        {
+            int index = ((int) chassisDef.Tonnage) / 5 - 1;
+            return index >= PartsByWeightTable.Count
+                ? PartsByWeightTable.Count
+                : index < 0
+                ? PartsByWeightTable[0]
+                : PartsByWeightTable[index];
+        }
+
+        public static int GetMechPartsRequired(this SimGameState simGameState, ChassisDef chassisDef)
+        {
+            return Settings.UseWeightThresholds 
+                ? GetMechPartsRequiredByWeight(chassisDef)
+                : simGameState.Constants.Story.DefaultMechPartMax;
+        }
+
+        public static int GetMechPartsRequired(this SimGameState simGameState, MechDef mechDef)
+        {
+            return simGameState.GetMechPartsRequired(mechDef.Chassis);
         }
     }
 }
